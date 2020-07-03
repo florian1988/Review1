dc5139e5ffab3bdc342ebd1a7b057c30651a0b79
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-07-03 17:07:17 +0200

    first commit

---
+++ src/JavaReview1.java
@@ -0,0 +1,46 @@
+public class JavaReview1 {
+
+    public static void RectArea(){
+        int länge1 = 20;
+        int breite1 = 30;
+        int länge2 = 40;
+        int breite2 = 50;
+
+        int area1 =  länge1 * breite1;
+        int area2 =  länge2 * breite2;
+
+        System.out.println("The area of the rectangular is " + area1);
+        System.out.println("The area of the rectangular is " + area2);
+
+    }
+
+
+
+
+    public static void calculateRectArea(int laenge, int breite){
+
+        int area = laenge * breite;
+
+        System.out.println("The area of the rectangular is " + area);
+    }
+
+
+    public static int rec(int l,int b){
+
+        int a = l * b;
+        return  a;
+    }
+
+
+    public static void main(String[] args) {
+
+        RectArea();
+
+       calculateRectArea(20,30);
+       calculateRectArea(40,50);
+
+        System.out.println("The area of the rectangular is " + rec(20,30));
+        System.out.println("The area of the rectangular is " + rec(40,50));
+
+    }
+}
---
+++ src/JavaReview2.java
@@ -0,0 +1,40 @@
+public class JavaReview2 {
+    /*
+    public static void square(){
+        int width = 16;
+        int area = width * width;
+        int perimeter = 4 * width;
+
+        System.out.println( "The area is " + area + "cm² and the perimeter is " + perimeter +"cm.");
+    }
+
+    public static void squaretwo( int width){
+
+        int area = width * width;
+        int perimeter = 4 * width;
+
+        System.out.println( "The area is " + area + "cm² and the perimeter is " + perimeter +"cm.");
+    }
+
+    public static String squarethree( int w ){
+
+        int area = w * w;
+        int perimeter = 4 * w;
+
+        return "The area is " + area + "cm² and the perimeter is " + perimeter +"cm.";
+    }
+
+
+    public static void main(String[] args) {
+        square();
+
+        squaretwo(16);
+
+        System.out.print(squarethree(16));
+
+    }
+
+     */
+   
+
+}

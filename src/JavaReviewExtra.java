d72efce1887850f8c23cd78de99e8168b36e4bb3
florian <flo.moerzinger@gmx.at>
2020-07-04 15:59:51 +0200

    bonus points

---
+++ src/JavaReviewExtra.java
@@ -0,0 +1,21 @@
+public class JavaReviewExtra {
+
+    public static void extra() {
+        int[] widths = {20, 30, 40, 50 };
+        int[] heights = {25, 35, 45, 55 };
+        int total = 0;
+        int t = 0;
+
+        for (int i = 0; i < widths.length; i++) {
+
+                total = widths[i] * heights[i];
+                 t = ++t;
+                System.out.println( "For the " + t + " rectangular is the area: " + total + "cm².");
+        }
+    }
+
+
+    public static void main(String[] args) {
+            extra();
+    }
+}
